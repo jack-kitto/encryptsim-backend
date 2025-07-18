b65d508b72942e729c8d6067e770ad7721e38aa2
Jack Kitto <jack.kitto@gmail.com>
2025-07-18 14:10:23 +0900

    feat: create environment configuration with validation

---
+++ src/env.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import { createEnv } from "@t3-oss/env-core";
+
+export const env = createEnv({
+  server: {
+    // Airalo
+    AIRALO_CLIENT_ID: z.string(),
+    AIRALO_CLIENT_URL: z.string().url(),
+    AIRALO_CLIENT_SECRET: z.string(),
+    USE_MOCK_AIRALO: z.enum(["true", "false"]),
+
+    // GCP/Firebase
+    // FIREBASE_DB_URL: z.string().url(),
+    // GCLOUD_PROJ_ID: z.string(),
+
+    // Quiknode
+    SOLANA_RPC_URL: z.string().url(),
+
+    // Solana public key
+    SOLANA_MASTER_PK: z.string(),
+
+    // DVPN
+    DVPN_API_KEY: z.string(),
+    DVPN_BASE_URL: z.string().url(),
+
+    // Server Config
+    // PORT: z.string().regex(/^\d+$/, "PORT must be a number"),
+  },
+  runtimeEnv: process.env,
+});
